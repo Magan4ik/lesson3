d7552e0bdab8326f04414cf8871d3951d012959f
Magan4ik <denissalnikov@ukr.net>
2023-11-22 03:34:49 +0200

    View that uses the service to get the weather

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+
+from services.weather.open_weather import WeatherInterface
 
 # Create your views here.
+def weather_widget_view(request: HttpRequest) -> HttpResponse:
+    weather = WeatherInterface("Kiev", country_code="UA")
+    weather_data = weather.get_weather_byindex(0)
+    context = {"weather": weather_data}
+    return render(request, "weather/weather_widget.html", context)
