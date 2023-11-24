b4590ee2a2448249c4a5f5b2b48cd77a16de8f38
Magan4ik <denissalnikov@ukr.net>
2023-11-24 13:56:42 +0200

    Added urls: city_follow, city_list, weather_detail, weather_search

--- weather/urls.py
+++ weather/urls.py
@@ -7,4 +7,8 @@ app_name = 'weather'
 
 urlpatterns = [
     path('', views.weather_widget_view, name='weather_widget'),
+    path('cityfollow/<str:city_name>/<int:city_id>/', views.city_follow_view, name='city_follow'),
+    path('cities/', views.city_list_view, name='city_list'),
+    path('detail/<int:city_id>/', views.weather_detail_view, name='weather_detail'),
+    path('search/', views.weather_search_view, name='weather_search'),
 ]
