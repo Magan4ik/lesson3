60b0ceaa45773e0a36661e6d8c781cde6f495553
Magan4ik <denissalnikov@ukr.net>
2023-11-24 13:35:31 +0200

    Created CityFollow model (Many-to-Many relationship between user and city)

--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from weather import models
 
 # Register your models here.
+@admin.register(models.CityFollow)
+class CityFollowAdmin(admin.ModelAdmin):
+    list_per_page = 25
+    list_display = ('city_name',)
+    list_filter = ('users', 'city_name')
--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class CityFollow(models.Model):
+    users = models.ManyToManyField(User, related_name="cities")
+    city_id = models.IntegerField(unique=True)
+    city_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.city_name
